2e675c79b7297dc90918d5206c930b27a7bdf1f5
Achim Rohn <achim@rohn.eu>
2015-07-19 20:50:12 +0200

    Now the run command with the albumart mount is actually working.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 mkdir -p $2/albumart
 chown -R 1000:1000 $1
 chown -R 1000:1000 $2
-sudo docker run -p 6600:6600 -p 16242:16242 -v $1:/home/groovebasin/music -v $2:/home/groovebasin/groove $2/albumart:/home/groovebasin/albumart --name groovebasin -it groovebasin
+sudo docker run -p 6600:6600 -p 16242:16242 -v $1:/home/groovebasin/music -v $2:/home/groovebasin/groove -v $2/albumart:/home/groovebasin/groovebasin/albumart --name groovebasin -it groovebasin
